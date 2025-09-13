4dd7ab7c36a7ff18eb307167077e459d3734ec19
mrKuncay008 <kuncaymuhammad@gmail.com>
2025-09-13 15:53:48 +0700

    feat: grandle update & navigation xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,17 @@ buildscript {
     repositories {
         google()
     }
+
     dependencies {
         val navVersion = "2.7.5"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
 
+
 plugins {
-//    alias(libs.plugins.android.application) apply false
-//    alias(libs.plugins.kotlin.android) apply false
-    id ("com.android.application") version "7.4.2" apply false
-    id ("com.android.library") version "7.4.2" apply false
+    id ("com.android.application") version "8.8.1" apply false
+    id ("com.android.library") version "8.8.1" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
